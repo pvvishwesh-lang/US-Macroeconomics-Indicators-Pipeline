18f7b6250ee450447ca57fb7ab6ffeb5902031e1
Vishwesh P V <pv.vishwesh@gmail.com>
2026-03-06 15:15:58 -0500

    Create 3_Monitoring_Log.py

---
+++ app/pages/3_Monitoring_Log.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from databricks.sdk import WorkspaceClient
+from databricks import sql
+import pandas as pd
+from datetime import datetime
+
+st.title("Monitoring Log")
+st.caption(f"Last updated: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+
+
+if "client" not in st.session_state:
+    st.session_state.client = WorkspaceClient(
+        host=st.secrets["DATABRICKS_HOST"],
+        token=st.secrets["DATABRICKS_TOKEN"]
+    )
+@st.cache_data(ttl=600)
+def get_monitoring_data():
+    conn = sql.connect(
+    server_hostname=st.secrets["DATABRICKS_HOST"].replace("https://",""),
+    http_path=st.secrets["HTTP_PATH"],
+    access_token=st.secrets["DATABRICKS_TOKEN"]
+    )
+
+    QUERY="SELECT * FROM us_macroeconomics_tracker.gold.monitoring_log ORDER BY obs_date DESC"
+    cursor = conn.cursor()
+
+    cursor.execute(QUERY)
+    results = cursor.fetchall()
+    cols=cursor.description
+    col_names = [col[0] for col in cols]
+    df = pd.DataFrame(results, columns=col_names)
+    conn.close()
+    return df
+if st.button("Refresh"):
+    st.cache_data.clear()
+
+df=get_monitoring_data()
+st.dataframe(df)
+
+drift_count = df[df['drift_flag'] == True].shape[0]
+st.metric(label="Drift Flags Detected", value=drift_count)
